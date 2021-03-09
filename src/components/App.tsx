773ce53d65167f1c9a7f9b388e642e413571133f
ronbarrantes <ronbarrantes@gmail.com>
2021-03-08 19:16:40 -0800

    Add App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react'
-import { css } from '@emotion/css'
+// import { css } from '@emotion/css'
 
-import Container from './Container'
-import Footer from './Footer'
-import Header from './Header'
+// import Container from './Container'
+// import Footer from './Footer'
+// import Header from './Header'
+
+// const App = (): JSX.Element => (
+//   <div className={appStyle} >
+//     <Header/>
+//     <Container/>
+//     <Footer/>
+//   </div>
+// )
 
 const App = (): JSX.Element => (
-  <div className={appStyle} >
-    <Header/>
-    <Container/>
-    <Footer/>
-  </div>
+  <div>Hello</div>
 )
 
-const appStyle = css`
-  /* background-color: green; */
-  width: 100vw;
-  display: grid;
-  min-height: 100vh;
-  grid-template-rows: 4rem 1fr 3rem;
-`
+// const appStyle = css`
+//   /* background-color: green; */
+//   width: 100vw;
+//   display: grid;
+//   min-height: 100vh;
+//   grid-template-rows: 4rem 1fr 3rem;
+// `
 
 export default App
